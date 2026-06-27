3dd67835583427685d97f3d4e61fe487c666badf
SergioTovar94 <sergiotovar9430@gmail.com>
2026-06-27 07:39:20 -0500

    Feature:Initialize Angular project with API client and models

---
+++ frontend/src/app/shared/components/status-pills/status-pills.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StatusPills } from './status-pills';
+
+describe('StatusPills', () => {
+  let component: StatusPills;
+  let fixture: ComponentFixture<StatusPills>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [StatusPills],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(StatusPills);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/shared/components/status-pills/status-pills.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-pills',
+  imports: [],
+  templateUrl: './status-pills.html',
+  styleUrl: './status-pills.scss',
+})
+export class StatusPills {}
